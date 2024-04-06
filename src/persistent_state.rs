98c5ea8a717eca16ec30f5686e84f579ab457dde
Simon Korzunov <twop@hey.com>
2024-04-06 00:45:15 -0400

    sketched out more of migration + bootstrapping

--- src/persistent_state.rs
+++ src/persistent_state.rs
@@ -3,10 +3,11 @@ use std::{
     path::PathBuf,
     time::{SystemTime, UNIX_EPOCH},
 };
+const CURRENT_VERSION: i32 = 2;
 
 use serde::{Deserialize, Serialize};
 
-#[derive(Debug, Deserialize, Serialize)]
+#[derive(Debug, Clone, Copy, Deserialize, Serialize)]
 enum NoteFile {
     Note(u32),
     Settings,
@@ -48,22 +49,25 @@ impl From<serde_json::Error> for SaveError {
     }
 }
 
-pub fn save<'a>(data: DataToSave<'a>, folder: PathBuf) -> Result<State, SaveError> {
+pub enum HydrationResult {
+    FolderIsMissing,
+    Success(RestoredData),
+}
+
+pub fn try_hydrate(number_of_notes: u32, folder: PathBuf) -> Result<HydrationResult, SaveError> {
+    todo!()
+}
+
+pub fn try_save<'a>(data: DataToSave<'a>, folder: PathBuf) -> Result<State, SaveError> {
     let DataToSave { files, selected } = data;
 
-    let start = SystemTime::now();
-    let now = start
-        .duration_since(UNIX_EPOCH)
-        .map(|d| d.as_millis())
-        .unwrap_or(0);
+    fs::create_dir_all(folder)?;
 
     let state = State {
-        version: 1,
-        last_saved: now,
+        version: CURRENT_VERSION,
+        last_saved: get_current_utc_timestamp(),
         selected,
     };
-
-    fs::create_dir_all(folder)?;
 
     fs::write(
         folder.join("state.json"),
@@ -80,6 +84,74 @@ pub fn save<'a>(data: DataToSave<'a>, folder: PathBuf) -> Result<State, SaveErro
     }
 
     Ok(state)
+}
+
+fn get_current_utc_timestamp() -> u128 {
+    let start = SystemTime::now();
+    let now = start
+        .duration_since(UNIX_EPOCH)
+        .map(|d| d.as_millis())
+        .unwrap_or(0);
+    now
+}
+
+pub fn fn_migrate_from_v1(old_state: &v1::PersistentState) -> (DataToSave, RestoredData) {
+    let selected = NoteFile::Note(old_state.selected_note);
+    let to_save = DataToSave {
+        files: old_state
+            .notes
+            .iter()
+            .enumerate()
+            .map(|(index, note)| (NoteFile::Note(index as u32), note.as_ref()))
+            // TODO settings note
+            // .chain([(NoteFile::Settings, "")])
+            .collect(),
+        selected,
+    };
+
+    let restored_data = RestoredData {
+        state: State {
+            version: CURRENT_VERSION,
+            last_saved: get_current_utc_timestamp(),
+            selected,
+        },
+        notes: old_state.notes.iter().map(|s| s.to_string()).collect(),
+        // TODO settings note
+        settings: "".to_string(),
+    };
+
+    (to_save, restored_data)
+}
+
+pub fn bootstrap(number_of_notes: u32) -> (DataToSave<'static>, RestoredData) {
+    let selected = NoteFile::Note(0);
+    let to_save = DataToSave {
+        // TODO fill out welcome notes
+        files: (0..number_of_notes)
+            .into_iter()
+            .map(|index| (NoteFile::Note(index as u32), ""))
+            // TODO settings note
+            // .chain([(NoteFile::Settings, "")])
+            .collect(),
+        selected,
+    };
+
+    // TODO fill out welcome notes
+    let restored_data = RestoredData {
+        state: State {
+            version: CURRENT_VERSION,
+            last_saved: get_current_utc_timestamp(),
+            selected,
+        },
+        notes: (0..number_of_notes)
+            .into_iter()
+            .map(|_| "".to_owned())
+            .collect(),
+        // TODO settings note
+        settings: "".to_string(),
+    };
+
+    (to_save, restored_data)
 }
 
 // ---------------------- older versions --------------
