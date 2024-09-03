ad6619273a8fb581d41a13947436fb9bef586291
Simon Korzunov <twop@hey.com>
2024-09-02 20:48:49 -0400

    first meaningful visual

--- site-rs/src/main.rs
+++ site-rs/src/main.rs
@@ -2,11 +2,17 @@
 
 use dioxus::prelude::*;
 use dioxus_logger::tracing::{info, Level};
+mod home_page;
+use home_page::HomePage;
 
 #[derive(Clone, Routable, Debug, PartialEq)]
 enum Route {
     #[route("/")]
     Home {},
+
+    #[route("/new")]
+    NewHome,
+
     #[route("/blog/:id")]
     Blog { id: i32 },
 }
@@ -33,15 +39,26 @@ fn Blog(id: i32) -> Element {
 }
 
 #[component]
+fn NewHome() -> Element {
+    rsx! {
+        HomePage {}
+        Link { to: Route::Home {}, "Go to counter" }
+    }
+}
+
+#[component]
 fn Home() -> Element {
     let mut count = use_signal(|| 0);
 
     rsx! {
         Link { to: Route::Blog { id: count() }, "Go to blog" }
+        Link { to: Route::NewHome, "Go to Marketing" }
         div { class: "container mx-auto px-4 py-8",
             h1 { "High-Five counter: {count}" }
-            button { onclick: move |_| count += 1, "Up high!" }
-            button { onclick: move |_| count -= 1, "Down low!" }
+            p {
+                button { onclick: move |_| count += 1, "Up high!" }
+                button { onclick: move |_| count -= 1, "Down low!" }
+            }
         }
     }
 }
